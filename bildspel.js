00e6416958e6bcc1a08599e1521ff30ea6d38661
eluxzz <kenttieu@hotmail.com>
2021-05-26 16:41:09 +0200

    Worked on JS, and made changes to animation
    Remove the animations for products on hover. Added a modal on click instead. Reoptimized pictures

--- bildspel.js
+++ bildspel.js
@@ -1,26 +1,10 @@
-var acc = document.getElementsByClassName("accordion");
 var currentBild = 0;
 var leftbtn = document.querySelector("#left");
 var rightbtn = document.querySelector("#right");
 var imageElement = document.getElementById("image");
-var navBtn;
 var images = ["3355", "3370", "3371", "3394","3405","3409"];
-// var timer = setInterval(nextImage, 6000, 1);
+var timer = setInterval(nextImage, 6000, 1);
 
-for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-            panel.style.margin = "0";
-            
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-            panel.style.margin = "-15px 0 0 0 ";
-        }
-    });
-}
 
 leftbtn.addEventListener("click", function(){
     nextImage(-1);
@@ -28,25 +12,6 @@ leftbtn.addEventListener("click", function(){
 rightbtn.addEventListener("click", function(){
     nextImage(1);
 });
-
-function init(){
-    navBtn = document.getElementById("nav_button");
-    navBtn.addEventListener("click", showMenu);
-}
-
-function showMenu(){
-    console.log("Test");
-    var nav = document.querySelector('nav');
-    if(nav.className === "open"){
-        nav.className = "closed";
-        navBtn.getElementById("bar").className = "fa fa-close";
-    }else{
-        nav.className = "open";
-        navBtn.getElementById("bar").className = "fa fa-bars";
-
-    }
-}
-
 
 /* 3355, 3364, 3371, 3394, 3405, 3409 */
 function nextImage(x){
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var acc = document.getElementsByClassName("accordion");
 var navBtn = document.getElementById("nav_button");
-
+var openModal = document.getElementsByClassName("open-modal");
+var modal = document.getElementById("modal");
+var modalImg = document.getElementById("modalImg")
 navBtn.addEventListener("click", showMenu);
+
 for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
@@ -14,6 +17,14 @@ for (let i = 0; i < acc.length; i++) {
             panel.style.maxHeight = panel.scrollHeight + "px";
             panel.style.margin = "-15px 0 0 0 ";
         }
+    });
+}
+
+for (let i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener("click", function () {
+        console.log("Ja");
+        modalImg.src = this.src;
+        modal.style.display = "block";
     });
 }
 
